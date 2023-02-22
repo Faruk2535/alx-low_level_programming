f9077c329bb0bad3442061ece4a098a5484ee502
faruk <farukonibudo@gmail.com>
2023-02-22 15:38:48 -0800

    betty edit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include "main.h"
-/*
-*_putchar = prints character
-*main = contains function that prints the char
-*return value = 0(return 0)
+/**
+ *main - the function return all characters called in it
+ *Return: return 0
 */
-
 int main(void)
 {
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-  return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,16 @@
 #include "main.h"
 
 /**
- *main= included a prototype function
- *
- *letter = prints letters in lowercase
- */
-
-
+*print_alphabet - print letter a-z in asccending order
+*letter = prints letters in lowercase
+*/
 void print_alphabet(void)
+/*short description - this prints alphabets*/
 {
-  char letter;
-    for(letter = 'a'; letter <= 'z'; letter++)
-      {
-	_putchar(letter);
-      }
-
-    _putchar('\n');
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
 }
