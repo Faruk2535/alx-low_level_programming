cdd6121b2b95701e48f2d44f064be1ba36dc9b72
faruk <farukonibudo@gmail.com>
2023-02-22 12:31:01 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,18 +3,18 @@
  *_putchar = prints character
  *
  *return value = 0(return 0)
-*/
+ */
 
 int main(void)
 {
-        _putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-        return (0);
+   _putchar('_');
+   _putchar('p');
+   _putchar('u');
+   _putchar('t');
+   _putchar('c');
+   _putchar('h');
+   _putchar('a');
+   _putchar('r');
+   _putchar('\n');
+   return (0);
 }
