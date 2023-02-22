a9b77ac0047692d7491572f88e23a7358c346473
faruk <farukonibudo@gmail.com>
2023-02-22 13:15:43 -0800

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /*
- *_putchar = prints character
- *main = contains function that prints the char
- *return value = 0(return 0)
- */
+*_putchar = prints character
+*main = contains function that prints the char
+*return value = 0(return 0)
+*/
 
 int main(void)
-
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return (0);
+  _putchar('_');
+  _putchar('p');
+  _putchar('u')
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+  return (0);
 }
