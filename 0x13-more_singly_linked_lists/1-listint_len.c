c46b4f80e737200053997833835ddf87de980123
faruk <farukonibudo@gmail.com>
2023-04-04 21:54:18 -0700

    new

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Print elements of a singly linked list.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t print_listint(const listint_t *h)
+{
+size_t count;
+listint_t *node = h;
+while(node!=NULL){
+
+count++;
+node=node->next;
+
+
+}
+return (count);
+}
